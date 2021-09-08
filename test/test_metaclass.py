8cbda29f4cd1fed741ddb0d4cc37d7412363b250
Erik Bernhardsson <mail@erikbern.com>
2021-09-08 18:08:35 -0400

    add breaking test with metaclasses

---
+++ test/test_metaclass.py
@@ -0,0 +1,42 @@
+import asyncio
+import time
+
+from synchronicity import Synchronizer
+
+SLEEP_DELAY = 0.1
+
+s = Synchronizer()
+
+class ObjectMetaclass(type):
+    def __new__(metacls, name, bases, dct):
+        print('creating new class', name)
+        new_cls = type.__new__(metacls, name, bases, dct)
+        print('new cls:', new_cls)
+        return s(new_cls)
+
+
+class ObjectBase(metaclass=ObjectMetaclass):
+    async def square(self, x):
+        await asyncio.sleep(SLEEP_DELAY)
+        return x**2
+
+
+class ObjectDerived(ObjectBase):
+    async def cube(self, x):
+        await asyncio.sleep(SLEE_DELAY)
+        return x**3
+
+
+def test_base():
+    base = ObjectBase()
+    t0 = time.time()
+    assert base.square(42) == 42*42
+    assert SLEEP_DELAY <= time.time() - t0 < 2 * SLEEP_DELAY
+
+    
+def test_derived():
+    derived = ObjectDerived()
+    t0 = time.time()
+    assert derived.square(42) == 42*42
+    assert derived.cube(42) == 42*42*42
+    assert SLEEP_DELAY <= time.time() - t0 < 2 * SLEEP_DELAY
