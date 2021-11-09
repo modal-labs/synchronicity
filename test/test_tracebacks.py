08f74c4eda2f785d1256666d79c7f72035907138
Erik Bernhardsson <mail@erikbern.com>
2021-11-09 16:49:32 -0500

    Simple test

---
+++ test/test_tracebacks.py
@@ -0,0 +1,29 @@
+import pytest
+import traceback
+
+from synchronicity import Synchronizer
+
+
+async def f_raises():
+    raise Exception("boom!")
+
+
+def analyze_tb(exc):
+    tb = exc.__traceback__
+    file_summary = {}
+    for frame in traceback.extract_tb(tb):
+        file_summary[frame.filename] = file_summary.get(frame.filename, 0) + 1
+    return file_summary
+
+
+def test_raises():
+    s = Synchronizer()
+    f_raises_s = s(f_raises)
+    with pytest.raises(Exception) as excinfo:
+        f_raises_s()
+
+    exc = excinfo.value
+    summary = analyze_tb(exc)
+
+    # Let's allow for at most 1 entry outside of this file
+    assert sum(summary.values()) <= summary[__file__] + 1
