a036987d3c4cd87de8396c2ce9bc7d8f42472d68
Erik Bernhardsson <mail@erikbern.com>
2021-11-09 22:49:33 -0500

    minor test cleanup

--- test/tracebacks_test.py
+++ test/tracebacks_test.py
@@ -19,17 +19,16 @@ async def gen():
 
 def check_traceback(exc):
     tb = exc.__traceback__
-    file_summary = {}
+    n_outside = 0
     for frame in traceback.extract_tb(tb):
-        file_summary[frame.filename] = file_summary.get(frame.filename, 0) + 1
+        if frame.filename != __file__:
+            n_outside += 1
 
     # Let's allow for at most 1 entry outside of this file
-    n = sum(file_summary.values())
-    k = file_summary[__file__]
-    if n > k + 1:
+    if n_outside > 1:
         for frame in traceback.extract_tb(tb):
             print(frame.filename, frame.lineno, frame.line)
-        raise Exception(f"Got {n} frames outside user code, expected {k} + 1")
+        raise Exception(f"Got {n_outside} frames outside user code, expected 1")
 
 
 def test_sync_to_async():
