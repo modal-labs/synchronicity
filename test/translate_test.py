519cd07f399be2a9e9c097a0eb3994d0ecb0fedf
Erik Bernhardsson <mail@erikbern.com>
2022-03-14 09:57:45 -0400

    Increase test scope

--- test/translate_test.py
+++ test/translate_test.py
@@ -19,8 +19,19 @@ def test_translate():
         def get(self):
             return self.foo
 
+        def set(self, foo):
+            self.foo = foo
+
+    Foo_blocking = s.get_blocking(Foo)
     FooProvider_blocking = s.get_blocking(FooProvider)
     foo_provider_blocking = FooProvider_blocking()
+
+    # Make sure two instances translated out are the same
     foo1 = foo_provider_blocking.get()
     foo2 = foo_provider_blocking.get()
     assert foo1 == foo2
+
+    # Translate an object in and then back out, make sure it's the same
+    foo = Foo_blocking()
+    foo_provider_blocking.set(foo)
+    assert foo_provider_blocking.get() == foo
