9eb5d93d6cd801e6a4d1bec6a36a4621b2d2422e
Erik Bernhardsson <mail@erikbern.com>
2022-03-22 22:46:05 -0400

    initial callback code

---
+++ test/callback_test.py
@@ -0,0 +1,38 @@
+import asyncio
+import pytest
+import time
+
+from synchronicity import Interface, Synchronizer
+from synchronicity.callback import Callback
+
+
+def sleep(t):
+    time.sleep(t)
+    return t
+
+
+async def sleep_async(t):
+    time.sleep(t)
+    return t
+
+
+@pytest.mark.asyncio
+async def test_blocking():
+    s = Synchronizer()
+    sleep_cb = Callback(s, Interface.BLOCKING, sleep)
+    t0 = time.time()
+    coros = [sleep_cb(i * 0.01) for i in range(1, 11)]
+    assert len(coros) == 10
+    rets = await asyncio.gather(*coros)
+    assert 0.095 <= time.time() - t0 <= 0.105
+
+
+@pytest.mark.asyncio
+async def test_async():
+    s = Synchronizer()
+    sleep_cb = Callback(s, Interface.ASYNC, sleep_async)
+    t0 = time.time()
+    coros = [sleep_cb(i * 0.01) for i in range(1, 11)]
+    assert len(coros) == 10
+    rets = await asyncio.gather(*coros)
+    assert 0.095 <= time.time() - t0 <= 0.105
