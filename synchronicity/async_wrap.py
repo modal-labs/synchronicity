1c662f9e0fcf24f9c0c005a88e5eb7235627b206
Elias Freider <elias@freider.se>
2023-03-23 16:32:37 +0100

    Adds type compatible asynccontextmanager factory function

--- synchronicity/async_wrap.py
+++ synchronicity/async_wrap.py
@@ -1,10 +1,12 @@
+import collections.abc
+import contextlib
 import functools
 import inspect
 import typing
 
 from .exceptions import UserCodeException
 from .interface import Interface
-
+from contextlib import asynccontextmanager as _asynccontextmanager
 
 def type_compat_wraps(func, interface: Interface, new_annotations=None):
     """Like functools.wraps but maintains `inspect.iscoroutinefunction` and allows custom type annotations overrides
@@ -38,3 +40,28 @@ def type_compat_wraps(func, interface: Interface, new_annotations=None):
 
             return wrapped
         return blockingfunc_deco
+
+
+
+
+YIELD_TYPE = typing.TypeVar("YIELD_TYPE")
+SEND_TYPE = typing.TypeVar("SEND_TYPE")
+
+def asynccontextmanager(f: typing.AsyncGenerator[YIELD_TYPE, SEND_TYPE]) -> typing.Callable[[], typing.AsyncContextManager[YIELD_TYPE]]:
+    """Wrapper around contextlib.asynccontextmanager that sets correct type annotations
+
+    The standard library one doesn't
+    """
+    acm_factory = _asynccontextmanager(f)
+
+    old_ret = acm_factory.__annotations__.pop("return", None)
+    if old_ret is not None:
+        if old_ret.__origin__ in [collections.abc.AsyncGenerator, collections.abc.AsyncIterator, collections.abc.AsyncIterator]:
+            acm_factory.__annotations__["return"] = typing.AsyncContextManager[old_ret.__args__[0]]
+        elif old_ret.__origin__ == contextlib.AbstractAsyncContextManager:
+            # if the standard lib fixes the annotations in the future, lets not break it...
+            return acm_factory
+    else:
+        raise ValueError("To use the fixed @asynccontextmanager, make sure to properly annotate your wrapped function as an AsyncGenerator")
+
+    return acm_factory
