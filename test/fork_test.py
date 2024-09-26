8975929f7b2c7628356748b0aec69c842e3c5bc8
Elias Freider <elias@modal.com>
2024-09-26 11:52:11 +0200

    Adds test

---
+++ test/fork_test.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+import subprocess
+import sys
+
+import pytest
+
+import synchronicity
+
+
+def test_fork_restarts_loop():
+    p = subprocess.Popen([sys.executable, Path(__file__).parent / "_forker.py"], encoding="utf8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        stdout, stderr = p.communicate(timeout=2)
+    except subprocess.TimeoutExpired:
+        p.kill()
+        assert False, "Fork process hanged"
+
+    assert p.returncode == 0
+    assert stdout == "done\ndone\n"
