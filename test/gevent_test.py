65ed8eb9e0a1be06eee8ce6352f1ca70f0f8dfc9
Elias Freider <elias@modal.com>
2024-10-01 12:05:08 +0200

    Fix test again

--- test/gevent_test.py
+++ test/gevent_test.py
@@ -1,10 +1,11 @@
 import os
+from pathlib import Path
 import subprocess
 import sys
 
 
 def test_gevent():
     # Run it in a separate process because gevent modifies a lot of modules
-    fn = os.path.join(os.path.dirname(__file__), "support" / "_gevent.py")
+    fn = Path(__file__).parent / "support" / "_gevent.py"
     ret = subprocess.run([sys.executable, fn], stdout=sys.stdout, stderr=sys.stderr, timeout=5)
     assert ret.returncode == 0
