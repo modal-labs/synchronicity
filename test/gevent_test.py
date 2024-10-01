cd45603a020781ff1bb6c3112243fa8c8f955de2
Elias Freider <elias@modal.com>
2024-10-01 12:03:52 +0200

    Fix test

--- test/gevent_test.py
+++ test/gevent_test.py
@@ -5,6 +5,6 @@ import sys
 
 def test_gevent():
     # Run it in a separate process because gevent modifies a lot of modules
-    fn = os.path.join(os.path.dirname(__file__), "_gevent.py")
+    fn = os.path.join(os.path.dirname(__file__), "support" / "_gevent.py")
     ret = subprocess.run([sys.executable, fn], stdout=sys.stdout, stderr=sys.stderr, timeout=5)
     assert ret.returncode == 0
--- test/shutdown_test.py
+++ test/shutdown_test.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import pytest
 import signal
 import subprocess
@@ -7,14 +8,14 @@ import sys
 
 def test_shutdown():
     # We run it in a separate process so we can simulate interrupting it
-    fn = os.path.join(os.path.dirname(__file__), "_shutdown.py")
+    fn = Path(__file__).parent / "support" / "_shutdown.py"
     p = subprocess.Popen(
         [sys.executable, fn],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         env={"PYTHONUNBUFFERED": "1"},
     )
-    for i in range(3):  # this number doesn't matter, it's a while loop
+    for i in range(2):  # this number doesn't matter, it's a while loop
         assert p.stdout.readline() == b"running\n"
     p.send_signal(signal.SIGINT)
     assert p.stdout.readline() == b"cancelled\n"
@@ -28,10 +29,47 @@ def test_shutdown():
     assert b"Traceback" not in stderr_content
 
 
-def test_keyboard_interrupt_doesnt_cancel(synchronizer):
+def test_keyboard_interrupt_reraised_as_is(synchronizer):
     @synchronizer.create_blocking
     async def a():
         raise KeyboardInterrupt()
 
     with pytest.raises(KeyboardInterrupt):
         a()
+
+
+def test_shutdown_during_ctx_mgr_setup():
+    # We run it in a separate process so we can simulate interrupting it
+    fn = Path(__file__).parent / "support" / "_shutdown_ctx_mgr.py"
+    p = subprocess.Popen(
+        [sys.executable, fn, "enter"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env={"PYTHONUNBUFFERED": "1"},
+    )
+    for i in range(2):  # this number doesn't matter, it's a while loop
+        assert p.stdout.readline() == b"enter\n"
+    p.send_signal(signal.SIGINT)
+    assert p.stdout.readline() == b"exit\n"
+    assert (
+        p.stdout.readline() == b"keyboard interrupt\n"
+    )
+    assert p.stderr.read() == b""
+
+def test_shutdown_during_ctx_mgr_yield():
+    # We run it in a separate process so we can simulate interrupting it
+    fn = Path(__file__).parent / "support" / "_shutdown_ctx_mgr.py"
+    p = subprocess.Popen(
+        [sys.executable, fn, "yield"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env={"PYTHONUNBUFFERED": "1"},
+    )
+    for i in range(2):  # this number doesn't matter, it's a while loop
+        assert p.stdout.readline() == b"in ctx\n"
+    p.send_signal(signal.SIGINT)
+    assert p.stdout.readline() == b"exit\n"
+    assert (
+        p.stdout.readline() == b"keyboard interrupt\n"
+    )
+    assert p.stderr.read() == b""
