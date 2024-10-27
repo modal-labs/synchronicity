647243abac57a56c18f6865afca71186ac590e29
Elias Freider <elias@modal.com>
2024-10-27 21:42:41 +0100

    Enable windows and mac tests in ci

--- test/shutdown_test.py
+++ test/shutdown_test.py
@@ -4,11 +4,32 @@ import subprocess
 import sys
 from pathlib import Path
 
+class PopenWithCtrlC(subprocess.Popen):
+    def __init__(self, *args, creationflags=0, **kwargs):
+        if sys.platform == "win32":
+            # needed on windows to separate ctrl-c lifecycle of subprocess from parent:
+            creationflags = creationflags | subprocess.CREATE_NEW_CONSOLE  # type: ignore
+
+        super().__init__(*args, **kwargs, creationflags=creationflags)
+
+    def send_ctrl_c(self):
+        # platform independent way to replicate the behavior of Ctrl-C:ing a cli app
+        if sys.platform == "win32":
+            # windows doesn't support sigint, and subprocess.CTRL_C_EVENT has a bunch
+            # of gotchas since it's bound to a console which is the same for the parent
+            # process by default, and can't be sent using the python standard library
+            # to a separate process's console
+            import console_ctrl
+
+            console_ctrl.send_ctrl_c(self.pid)  # noqa [E731]
+        else:
+            self.send_signal(signal.SIGINT)
+
 
 def test_shutdown():
     # We run it in a separate process so we can simulate interrupting it
     fn = Path(__file__).parent / "support" / "_shutdown.py"
-    p = subprocess.Popen(
+    p = PopenWithCtrlC(
         [sys.executable, fn],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -16,7 +37,7 @@ def test_shutdown():
     )
     for i in range(2):  # this number doesn't matter, it's a while loop
         assert p.stdout.readline() == b"running\n"
-    p.send_signal(signal.SIGINT)
+    p.send_ctrl_c()
     assert p.stdout.readline() == b"cancelled\n"
     assert p.stdout.readline() == b"handled cancellation\n"
     assert p.stdout.readline() == b"exit async\n"
@@ -40,7 +61,7 @@ def test_keyboard_interrupt_reraised_as_is(synchronizer):
 def test_shutdown_during_ctx_mgr_setup():
     # We run it in a separate process so we can simulate interrupting it
     fn = Path(__file__).parent / "support" / "_shutdown_ctx_mgr.py"
-    p = subprocess.Popen(
+    p = PopenWithCtrlC(
         [sys.executable, fn, "enter"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -48,7 +69,7 @@ def test_shutdown_during_ctx_mgr_setup():
     )
     for i in range(2):  # this number doesn't matter, it's a while loop
         assert p.stdout.readline() == b"enter\n"
-    p.send_signal(signal.SIGINT)
+    p.send_ctrl_c()
     assert p.stdout.readline() == b"exit\n"
     assert p.stdout.readline() == b"keyboard interrupt\n"
     assert p.stderr.read() == b""
@@ -57,7 +78,7 @@ def test_shutdown_during_ctx_mgr_setup():
 def test_shutdown_during_ctx_mgr_yield():
     # We run it in a separate process so we can simulate interrupting it
     fn = Path(__file__).parent / "support" / "_shutdown_ctx_mgr.py"
-    p = subprocess.Popen(
+    p = PopenWithCtrlC(
         [sys.executable, fn, "yield"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -65,7 +86,7 @@ def test_shutdown_during_ctx_mgr_yield():
     )
     for i in range(2):  # this number doesn't matter, it's a while loop
         assert p.stdout.readline() == b"in ctx\n"
-    p.send_signal(signal.SIGINT)
+    p.send_ctrl_c()
     assert p.stdout.readline() == b"exit\n"
     assert p.stdout.readline() == b"keyboard interrupt\n"
     assert p.stderr.read() == b""
@@ -73,7 +94,7 @@ def test_shutdown_during_ctx_mgr_yield():
 
 def test_shutdown_during_async_run():
     fn = Path(__file__).parent / "support" / "_shutdown_async_run.py"
-    p = subprocess.Popen(
+    p = PopenWithCtrlC(
         [sys.executable, fn],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -82,7 +103,7 @@ def test_shutdown_during_async_run():
     )
     for i in range(2):  # this number doesn't matter, it's a while loop
         assert p.stdout.readline() == "running\n"
-    p.send_signal(signal.SIGINT)
+    p.send_ctrl_c()
     assert p.stdout.readline() == "cancelled\n"
     assert p.stdout.readline() == "handled cancellation\n"
     assert p.stdout.readline() == "exit async\n"
