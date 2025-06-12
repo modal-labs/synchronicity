604ef387a27f71071362834305e68cc5bf30d093
Elias Freider <elias@modal.com>
2025-06-12 11:25:12 +0200

    debug event loop cleanup

--- test/conftest.py
+++ test/conftest.py
@@ -4,7 +4,14 @@ from synchronicity import Synchronizer
 
 
 @pytest.fixture()
-def synchronizer():
+def synchronizer(request):
     s = Synchronizer()
+    loop = s._get_loop(start=True)
+
+    def custom_repr(self):
+        return request.node.name
+
+    loop.get_debug = custom_repr.__get__(loop)  # haxx
     yield s
+    print("closing synchronizer for test", request.node.name)
     s._close_loop()  # avoid "unclosed event loop" warnings in tests when garbage collecting synchronizers
