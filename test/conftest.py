383e694eb189a8a380390f9957efa22da3168392
Elias Freider <elias@modal.com>
2025-06-17 09:24:26 +0200

    Always use asyncio debug

--- test/conftest.py
+++ test/conftest.py
@@ -3,9 +3,13 @@ import pytest
 from synchronicity import Synchronizer
 
 
-@pytest.fixture()
-def synchronizer(monkeypatch):
+@pytest.fixture(autouse=True)
+def use_asyncio_debug(monkeypatch):
     monkeypatch.setenv("PYTHONASYNCIODEBUG", "1")
+
+
+@pytest.fixture()
+def synchronizer(use_asyncio_debug):
     s = Synchronizer()
     yield s
     s._close_loop()  # avoid "unclosed event loop" warnings in tests when garbage collecting synchronizers
