c62b29b0fdfbead85461a87abed57aa05167b896
Elias Freider <elias@modal.com>
2025-07-15 13:48:52 +0200

    Vibe coding

--- src/synchronicity2/synchronizer.py
+++ src/synchronicity2/synchronizer.py
@@ -1,13 +1,130 @@
 import asyncio
+import atexit
 import concurrent.futures
+import os
+import threading
 import types
 import typing
 from typing import Callable, Optional
 
 T = typing.TypeVar("T", bound=typing.Union[type, Callable])
 
+# Global registry for synchronizer instances
+_synchronizer_registry = {}
+
+
+def get_synchronizer(name: str) -> "Synchronizer":
+    """Get or create a synchronizer instance by name from the global registry."""
+    if name not in _synchronizer_registry:
+        _synchronizer_registry[name] = Synchronizer()
+    return _synchronizer_registry[name]
+
 
 class Synchronizer:
+    def __init__(self):
+        self._future_poll_interval = 0.1
+        self._loop = None
+        self._loop_creation_lock = threading.Lock()
+        self._thread = None
+        self._owner_pid = None
+        self._stopping = None
+
+        # Special attribute we use to go from wrapped <-> original
+        self._wrapped_attr = "_sync_wrapped_%d" % id(self)
+        self._original_attr = "_sync_original_%d" % id(self)
+
+        # Special attribute to mark something as non-wrappable
+        self._nowrap_attr = "_sync_nonwrap_%d" % id(self)
+        self._input_translation_attr = "_sync_input_translation_%d" % id(self)
+        self._output_translation_attr = "_sync_output_translation_%d" % id(self)
+
+        atexit.register(self._close_loop)
+
+    _PICKLE_ATTRS = [
+        "_multiwrap_warning",
+        "_async_leakage_warning",
+    ]
+
+    def __getstate__(self):
+        return dict([(attr, getattr(self, attr)) for attr in self._PICKLE_ATTRS])
+
+    def __setstate__(self, d):
+        for attr in self._PICKLE_ATTRS:
+            setattr(self, attr, d[attr])
+
+    def _start_loop(self):
+        with self._loop_creation_lock:
+            if self._loop and self._loop.is_running():
+                # in case of a race between two _start_loop, the loop might already
+                # be created here by another thread
+                return self._loop
+
+            is_ready = threading.Event()
+
+            def thread_inner():
+                async def loop_inner():
+                    self._loop = asyncio.get_running_loop()
+                    self._stopping = asyncio.Event()
+                    is_ready.set()
+                    await self._stopping.wait()  # wait until told to stop
+
+                try:
+                    asyncio.run(loop_inner())
+                except RuntimeError as exc:
+                    # Python 3.12 raises a RuntimeError when new threads are created at shutdown.
+                    # Swallowing it here is innocuous, but ideally we will revisit this after
+                    # refactoring the shutdown handlers that modal uses to avoid triggering it.
+                    if "can't create new thread at interpreter shutdown" not in str(exc):
+                        raise exc
+
+            self._owner_pid = os.getpid()
+            thread = threading.Thread(target=thread_inner, daemon=True)
+            thread.start()
+            is_ready.wait()  # TODO: this might block for a very short time
+            self._thread = thread
+            return self._loop
+
+    def _close_loop(self):
+        # Use getattr to protect against weird gc races when we get here via __del__
+        if getattr(self, "_thread", None) is not None:
+            if not self._loop.is_closed():
+                # This also serves the purpose of waking up an idle loop
+                self._loop.call_soon_threadsafe(self._stopping.set)
+            self._thread.join()
+            self._thread = None
+            self._loop = None
+            self._owner_pid = None
+
+    def _get_loop(self, start=False) -> asyncio.AbstractEventLoop:
+        if self._thread and not self._thread.is_alive():
+            if self._owner_pid == os.getpid():
+                # warn - thread died without us forking
+                raise RuntimeError("Synchronizer thread unexpectedly died")
+
+            self._thread = None
+            self._loop = None
+
+        if self._loop is None and start:
+            return self._start_loop()
+        return self._loop
+
+    def _get_running_loop(self):
+        # TODO: delete this method
+        try:
+            return asyncio.get_running_loop()
+        except RuntimeError:
+            return
+
+    def _is_inside_loop(self):
+        loop = self._get_loop()
+        if loop is None:
+            return False
+        if threading.current_thread() != self._thread:
+            # gevent does something bad that causes asyncio.get_running_loop() to return self._loop
+            return False
+        current_loop = self._get_running_loop()
+        return loop == current_loop
+
     def _run_function_sync(self, coro):
         if self._is_inside_loop():
             raise Exception("Deadlock detected: calling a sync function from the synchronizer loop")
@@ -199,7 +316,7 @@ class Library:
         # Generate the sync function source code
         sync_func_name = f.__name__
         sync_code = f"""def {sync_func_name}({param_str}):
-    return _synchronizer.{method_name}({f.__name__}({param_str}), {f.__name__})"""
+    return get_synchronizer('{self._synchronizer_name}').{method_name}({f.__name__}({param_str}), {f.__name__})"""
 
         return sync_code
 
