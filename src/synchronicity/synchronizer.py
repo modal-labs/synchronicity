7a2275e169b3ffe0b73bd670bca1f33df5c5189e
Freddy Boulton <41651716+freddyaboulton@users.noreply.github.com>
2026-06-17 14:42:35 -0400

    Fix

--- src/synchronicity/synchronizer.py
+++ src/synchronicity/synchronizer.py
@@ -811,6 +811,8 @@ Traceback:{self._thread_traceback}"""
             try:
                 instance = self.__dict__[synchronizer_self._original_attr]
             except (KeyError, AttributeError):
+                # sdk671 - If self is not the wrapped class, fallback to calling
+                # the wrapped method on self
                 return wrapped_method(self, *args, **kwargs)
             with suppress_synchronicity_tb_frames():
                 try:
